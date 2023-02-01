76c7ac6e16f3c01d1523fda439e021e0e1c0ce39
Dwayne Robinson <dwayner@microsoft.com>
2023-01-31 17:50:55 -0800

    Minor typo

--- common/TextTreeParser.h
+++ common/TextTreeParser.h
@@ -151,7 +151,7 @@ public:
     void Clear();
     uint32_t GetNodeCount() const throw();
     bool empty() const throw(); // Node there exists a virtual root. So after calling ReadNodes, it will be non-empty even if the file was empty.
-    iterator begin();   // Iterator warks entire tree, top down (pre-order).
+    iterator begin();   // Iterator walks entire tree, top down (pre-order).
     iterator end();
     iterator BeginFirstChild(); // You cannot deference this iterator, but you can use it to search from.
 
